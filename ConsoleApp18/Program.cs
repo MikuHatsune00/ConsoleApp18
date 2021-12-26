afb7f90a112a559eaeadb47f4da739954147ec50
Kseniia <balerinagizelle@mail.ru>
2021-12-26 17:28:01 +0300

    Add project files.

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "({}[]())";
+            Console.WriteLine(Check(myString));
+            Console.ReadKey();
+        }
+        static bool Check(string myString)
+        {
+            Stack<char> myStack = new Stack<char>();
+            Dictionary<char, char> brackets = new Dictionary<char, char>
+            {
+                { '(',')'},
+                { '[',']'},
+                { '{','}'},
+
+            };
+            foreach (char c in myString)
+            {
+                if (c == '(' || c == '[' || c == '{')
+                {
+                    myStack.Push(brackets[c]);
+                }
+                if (c == ')' || c == ']' || c == '}')
+                {
+                    if (myStack.Count == 0  || myStack.Pop() != c)
+                    {
+                        return false;
+                    }
+                }
+            }
+            if (myStack.Count == 0)
+            { return true; }
+            else
+                return false;
+        }
+    }
+}
